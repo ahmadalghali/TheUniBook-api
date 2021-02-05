038a4012d13c69b93f3b4b104b983cbb1356b3c2
ahmadalghali <ahmad-mustafaalghali@hotmail.com>
2021-02-05 19:45:33 +0000

    inital commit

---
+++ src/main/java/com/greenwich/theunibook/TheunibookApplication.java
@@ -0,0 +1,13 @@
+package com.greenwich.theunibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheunibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheunibookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenwich/theunibook/TheunibookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenwich.theunibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheunibookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
