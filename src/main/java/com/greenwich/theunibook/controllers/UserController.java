0ecba9a6485b2c71ef3fa7d345f398516ff2a048
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-03-07 22:46:50 +0000

    password hashing

--- src/main/java/com/greenwich/theunibook/controllers/UserController.java
+++ src/main/java/com/greenwich/theunibook/controllers/UserController.java
@@ -9,6 +9,8 @@ import com.greenwich.theunibook.web.responses.RegisterResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/register")
-    public RegisterResponse register(@RequestBody RegisterRequest registerRequest) {
+    public RegisterResponse register(@RequestBody RegisterRequest registerRequest) throws NoSuchAlgorithmException, InvalidKeySpecException {
         return userService.register(registerRequest);
     }
 
@@ -29,7 +31,7 @@ public class UserController {
 //    }
 
     @PostMapping("/login")
-    public HashMap<String, Object> login(@RequestBody LoginRequest loginRequest) {
+    public HashMap<String, Object> login(@RequestBody LoginRequest loginRequest) throws NoSuchAlgorithmException, InvalidKeySpecException {
         return userService.login(loginRequest);
     }
 
--- src/main/java/com/greenwich/theunibook/services/UserService.java
+++ src/main/java/com/greenwich/theunibook/services/UserService.java
@@ -1,9 +1,7 @@
 package com.greenwich.theunibook.services;
 
-import com.greenwich.theunibook.dto.IdeaDTO;
 import com.greenwich.theunibook.dto.UserDTO;
 import com.greenwich.theunibook.enums.UserRole;
-import com.greenwich.theunibook.models.Idea;
 import com.greenwich.theunibook.models.User;
 import com.greenwich.theunibook.repository.DepartmentRepository;
 import com.greenwich.theunibook.repository.UserRepository;
@@ -14,8 +12,13 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -32,15 +35,18 @@ public class UserService {
     private ModelMapper modelMapper = new ModelMapper();
 
 
-    public RegisterResponse register(RegisterRequest registerRequest) {
+    public RegisterResponse register(RegisterRequest registerRequest) throws NoSuchAlgorithmException, InvalidKeySpecException {
         RegisterResponse registerResponse = new RegisterResponse();
-
+        
+        String inputPassword = registerRequest.getPassword() ;
+        String generatePasswordHash = generatePasswordHash(inputPassword);
+        
         if (userRepository.findByEmail(registerRequest.getEmail()) != null) {
             registerResponse.setMessage("user exists");
             registerResponse.setUser(null);
         } else {
             User user = new User(registerRequest.getFirstname(), registerRequest.getLastname(),
-                    registerRequest.getEmail(), registerRequest.getPassword(), registerRequest.getDepartmentId());
+                    registerRequest.getEmail(), generatePasswordHash, registerRequest.getDepartmentId());
             user.setRole(UserRole.STAFF);
 //            User user = new User(registerRequest.getEmail(), registerRequest.getPassword());
 
@@ -61,7 +67,85 @@ public class UserService {
         return registerResponse;
     }
 
+    public HashMap<String, Object> login(LoginRequest loginRequest) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
+        HashMap<String, Object> loginResponse = new HashMap();
+
+        User user = userRepository.findByEmail(loginRequest.getEmail());
+
+        boolean userExists = user != null;
+
+        String storedUserPassword = user.getPassword();
+
+        String loginInputPassword = loginRequest.getPassword();
+        String generateLoginHash = generatePasswordHash(loginInputPassword);
+
+        if (userExists) {
+            boolean passwordMatches = storedUserPassword.equals(generateLoginHash);
+
+            if (passwordMatches) {
+
+                loginResponse.put("user", convertToUserْDTO(user));
+
+                loginResponse.put("message", "logged in");
+
+            } else {
+
+                loginResponse.put("message", "bad credentials");
+            }
+
+        } else {
+            loginResponse.put("message", "bad credentials - user doesnt exist");
+        }
+
+        return loginResponse;
+    }
+
+    private String generatePasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String generatedPassword = null;
+        //password salting
+        String fixedBytes = "17 f6 42 59 0d 09 5b 3c ba b7 f5 8b 36 bd 3c 15";
+        byte[] salt = fixedBytes.getBytes(StandardCharsets.UTF_8);
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add the salt
+            md.update(salt);
+
+            byte[] bytes = md.digest(password.getBytes());
+
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    private UserDTO convertToUserْDTO(User user) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+        userDTO.setDepartment(departmentRepository.findById(user.getDepartmentId()).get());
+
+        return userDTO;
+    }
+
+    public List<User> getAllUsers() {
+
+        return userRepository.getAllUsers();
+    }
+
+
+    public User getUser(int userId) {
+        return userRepository.findById(userId).get();
+    }
 //    public LoginResponse login(LoginRequest loginRequest) {
 //
 //        LoginResponse loginResponse = new LoginResponse();
@@ -92,57 +176,5 @@ public class UserService {
 //
 //        return loginResponse;
 //    }
-
-
-    private UserDTO convertToUserْDTO(User user) {
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
-        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
-        userDTO.setDepartment(departmentRepository.findById(user.getDepartmentId()).get());
-
-        return userDTO;
-    }
-
-
-    public HashMap<String, Object> login(LoginRequest loginRequest) {
-
-        HashMap<String, Object> loginResponse = new HashMap();
-
-        User user = userRepository.findByEmail(loginRequest.getEmail());
-
-        boolean userExists = user != null;
-
-
-        if (userExists) {
-            boolean passwordMatches = user.getPassword().trim().equals(loginRequest.getPassword());
-
-            if (passwordMatches) {
-
-                loginResponse.put("user", convertToUserْDTO(user));
-//                loginResponse.put("department", departmentRepository.findById(user.getDepartmentId()).get());
-                loginResponse.put("message", "logged in");
-
-            } else {
-
-                loginResponse.put("message", "bad credentials");
-            }
-
-        } else {
-            loginResponse.put("message", "bad credentials - user doesnt exist");
-        }
-
-        return loginResponse;
-    }
-
-
-    public List<User> getAllUsers() {
-
-        return userRepository.getAllUsers();
-    }
-
-
-    public User getUser(int userId) {
-        return userRepository.findById(userId).get();
-    }
 
 }
