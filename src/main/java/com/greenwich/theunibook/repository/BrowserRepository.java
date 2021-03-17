69554c1dc529c5178a71b942ad9a537934a59dad
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-03-17 02:44:50 +0000

    browser

--- src/main/java/com/greenwich/theunibook/repository/BrowserRepository.java
+++ src/main/java/com/greenwich/theunibook/repository/BrowserRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface BrowserRepository extends CrudRepository<Browser, Integer> {
 
-    @Query("SELECT TOP 10 FROM browsers ORDER BY times_used DESC")
+    @Query("SELECT TOP 10 * FROM browsers ORDER BY times_used DESC")
     List<Browser> getAllBrowsers();
 
     @Query("SELECT * FROM browsers WHERE browser_name = :browserName")
